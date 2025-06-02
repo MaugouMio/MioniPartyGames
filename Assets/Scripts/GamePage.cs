dcbaf0dae5723a24397f184405c1542fb34716c2
MaugouMio <maugoumine@gmail.com>
2025-06-02 12:14:11 +0800

    畫面中間物件顯示串接

--- Assets/Scripts/GamePage.cs
+++ Assets/Scripts/GamePage.cs
@@ -9,6 +9,34 @@ public class GamePage : MonoBehaviour
 
 	[SerializeField]
 	private List<PlayerInfo> PlayerList;
+
+	[SerializeField]
+	private GameObject IdlePage;
+
+	[SerializeField]
+	private GameObject QuestionPage;
+	[SerializeField]
+	private GameObject PlayerQuestionPage;
+	[SerializeField]
+	private Text QuestionTargetPlayer;
+	[SerializeField]
+	private InputField QuestionInput;
+	[SerializeField]
+	private GameObject SpectateQuestionPage;
+
+	[SerializeField]
+	private GameObject GuessingPage;
+	[SerializeField]
+	private Text GuessingPlayer;
+	[SerializeField]
+	private Text GuessedText;
+	[SerializeField]
+	private InputField GuessInput;
+	[SerializeField]
+	private GameObject GuessConfirmButtons;
+	[SerializeField]
+	private GameObject VoteButtons;
+
 	[SerializeField]
 	private TextList EventList;
 	[SerializeField]
@@ -96,7 +124,54 @@ public class GamePage : MonoBehaviour
 
 	public void UpdateMiddlePage()
 	{
+		IdlePage.SetActive(GameData.Instance.CurrentState == GameState.WAITING);
+		QuestionPage.SetActive(GameData.Instance.CurrentState == GameState.PREPARING);
+		GuessingPage.SetActive(GameData.Instance.CurrentState == GameState.GUESSING || GameData.Instance.CurrentState == GameState.VOTING);
 
+		switch (GameData.Instance.CurrentState)
+		{
+			case GameState.PREPARING:
+				{
+					int selfIndex = GameData.Instance.PlayerOrder.IndexOf(GameData.Instance.SelfUID);
+					PlayerQuestionPage.SetActive(selfIndex >= 0);
+					SpectateQuestionPage.SetActive(selfIndex < 0);
+					if (selfIndex >= 0)
+					{
+						ushort nextPlayerUID = GameData.Instance.PlayerOrder[(selfIndex + 1) % GameData.Instance.PlayerOrder.Count];
+						QuestionTargetPlayer.text = GameData.Instance.UserDatas[nextPlayerUID].Name;
+					}
+				}
+				break;
+			case GameState.GUESSING:
+				{
+					ushort guessingPlayerUID = GameData.Instance.PlayerOrder[GameData.Instance.GuessingPlayerIndex];
+					GuessingPlayer.text = GameData.Instance.UserDatas[guessingPlayerUID].Name;
+
+					bool isSelfGuessing = guessingPlayerUID == GameData.Instance.SelfUID;
+					GuessedText.gameObject.SetActive(!isSelfGuessing);
+					GuessedText.text = "____";
+					GuessInput.gameObject.SetActive(isSelfGuessing);
+
+					GuessConfirmButtons.SetActive(isSelfGuessing);
+					VoteButtons.SetActive(false);
+				}
+				break;
+			case GameState.VOTING:
+				{
+					ushort guessingPlayerUID = GameData.Instance.PlayerOrder[GameData.Instance.GuessingPlayerIndex];
+					GuessingPlayer.text = GameData.Instance.UserDatas[guessingPlayerUID].Name;
+
+					GuessedText.gameObject.SetActive(true);
+					GuessedText.text = GameData.Instance.VotingGuess;
+					GuessInput.gameObject.SetActive(false);
+
+					GuessConfirmButtons.SetActive(false);
+					VoteButtons.SetActive(guessingPlayerUID != GameData.Instance.SelfUID);
+				}
+				break;
+			default:
+				break;
+		}
 	}
 
 	public void UpdateEventList()
