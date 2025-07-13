f6f68664b89dc9c6e82feae83d87e139d66cf22e
MaugouMio <maugoumine@gmail.com>
2025-07-13 12:33:55 +0800

    隨機房間 ID 生成器

--- Server/id_generator.py
+++ Server/id_generator.py
@@ -1,24 +1,25 @@
 import heapq
+import random
 
 
 class _SerialIDGenerator:
 	def __init__(self, max_id):
-		self._uid_serial = 0
-		self._free_uid = []
+		self._id_serial = 0
+		self._free_id_list = []
 		
 		self._max_id = max_id
 
 	def generate(self):
-		if len(self._free_uid) > 0:
-			return heapq.heappop(self._free_uid)
+		if len(self._free_id_list) > 0:
+			return heapq.heappop(self._free_id_list)
 		
-		self._uid_serial += 1
-		if self._uid_serial > self._max_id:
+		self._id_serial += 1
+		if self._id_serial > self._max_id:
 			return -1
-		return self._uid_serial
+		return self._id_serial
 
-	def release(self, uid):
-		heapq.heappush(self._free_uid, uid)
+	def release(self, id):
+		heapq.heappush(self._free_id_list, id)
 
 _player_uid_generator = _SerialIDGenerator(0xffff)
 
@@ -27,3 +28,30 @@ def generate_player_uid():
 
 def release_player_uid(uid):
 	_player_uid_generator.release(uid)
+
+
+class _RandomIDGenerator:
+	def __init__(self, max_id):
+		self._available_id_list = list(range(1, max_id + 1))
+	
+	def generate(self):
+		max_index = len(self._available_id_list) - 1
+		if max_index < 0:
+			return -1
+		
+		index = random.randint(0, max_index)
+		self._available_id_list[index], self._available_id_list[max_index] = (
+			self._available_id_list[max_index], self._available_id_list[index]
+		)
+		return self._available_id_list.pop()
+	
+	def release(self, id):
+		self._available_id_list.append(id)
+
+_room_id_generator = _RandomIDGenerator(99999)
+
+def generate_room_id():
+	return _room_id_generator.generate()
+
+def release_room_id(id):
+	_room_id_generator.release(id)
